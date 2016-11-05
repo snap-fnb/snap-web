1eb491c734df58c6977ea375a5f9423f8184bdfb
Matt Payne <Payne@MattPayne.org>
2016-11-05 16:07:01 -0500

    {{chat-box}} look improved with Tim's help.  Thanks, Tim

--- app/pods/components/chat-box/component.js
+++ app/pods/components/chat-box/component.js
@@ -7,14 +7,14 @@ export default Ember.Component.extend({
     widgetState : 'closed|closed', // Have the component start opened or closed
 
     headerText: 'How can we help?', // Text in the widget header
-    headerImage: 'https://s.gravatar.com/avatar/2f96c7e4839ffc2faf65052234f534f0?s=100', // Path to profile image
+    headerImage: 'assets/images/fnb_circle_logo.png', // Path to profile image
 
     emptyChatTopic: 'Support chat', // Topic to show inside empty chat
     emptyChatText: 'Questions? Just type it below and we\'ll answer you..', // Text to show inside empty chat
 
     inputPlaceholderText: 'Write message here...', // Placeholder for the text input
 
-    supportUsername: 'Wingmen', // Name to show support replies under
+    supportUsername: 'FNB Advisor: Tracey', // Name to show support replies under
     chatUserName: 'Me', // Name to show before user messages
   }
 });
