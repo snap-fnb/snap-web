c5a06357e78b062d20474ee897c7b39798bc5959
Matt Payne <Payne@MattPayne.org>
2016-11-05 15:00:09 -0500

    The {{chat-box}} component works.  It needs to be styled, please
    To have it work you need to have https://github.com/snap-fnb/ember-cli-slack-chat-node-server
    running locally.  See that projects for how to launch it.

--- app/pods/components/chat-box/component.js
+++ app/pods/components/chat-box/component.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  options:
+  {
+    serverUrl: 'http://localhost:1337', // Url to your Slack server component
+    widgetState : 'closed|closed', // Have the component start opened or closed
+
+    headerText: 'How can we help?', // Text in the widget header
+    headerImage: 'https://s.gravatar.com/avatar/2f96c7e4839ffc2faf65052234f534f0?s=100', // Path to profile image
+
+    emptyChatTopic: 'Support chat', // Topic to show inside empty chat
+    emptyChatText: 'Questions? Just type it below and we\'ll answer you..', // Text to show inside empty chat
+
+    inputPlaceholderText: 'Write message here...', // Placeholder for the text input
+
+    supportUsername: 'Wingmen', // Name to show support replies under
+    chatUserName: 'Me', // Name to show before user messages
+  }
 });
