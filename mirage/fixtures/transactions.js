d5fa37c29d8a9ddd7cc1702c327f71c81c1b8581
Steve Chikwaya <stavarotti@gmail.com>
2016-11-06 16:16:02 -0600

    Updates images

--- mirage/fixtures/transactions.js
+++ mirage/fixtures/transactions.js
@@ -1,45 +1,5 @@
 export default [{
   "kind": "transaction",
-  "amount": 1234.56,
-  "flow": "in",
-  "payee": "State of Nebraska",
-  "account": "joint checking",
-  "category": "income",
-  "memo": "Pay day is always awesome!",
-  "timeStamp": "2016-10-02T18:25:43.511Z",
-  "tags": [
-    "income"
-  ]
-}, {
-  "kind": "transaction",
-  "amount": 142.86,
-  "flow": "out",
-  "payee": "Wooly Canada",
-  "account": "joint checking",
-  "category": "clothing",
-  "memo": "Amazing wool sweater",
-  "timeStamp": "2016-09-22T18:25:43.511Z",
-  "tags": [
-    "trip",
-    "canada",
-    "clothes"
-  ]
-}, {
-  "kind": "transaction",
-  "amount": 72.11,
-  "flow": "out",
-  "payee": "Delicious Canada",
-  "account": "joint checking",
-  "category": "travel",
-  "memo": "Great meal - fresh salmon",
-  "timeStamp": "2016-06-23T18:25:43.511Z",
-  "tags": [
-    "trip",
-    "canada",
-    "dining"
-  ]
-}, {
-  "kind": "transaction",
   "amount": 12.56,
   "flow": "out",
   "payee": "Block 16",
@@ -89,6 +49,72 @@ export default [{
   "tags": [
     "dining",
     "lunch"
+  ]
+}, {
+  "kind": "transaction",
+  "amount": 6.00,
+  "flow": "out",
+  "payee": "Five Guys",
+  "account": "steve",
+  "category": "dining",
+  "memo": "I don't get the hoopla, it was aight.",
+  "timeStamp": "2016-11-05T18:25:43.511Z",
+  "tags": [
+    "dining",
+    "lunch"
+  ]
+}, {
+  "kind": "transaction",
+  "amount": 12.00,
+  "flow": "out",
+  "payee": "Fat Shack",
+  "account": "steve",
+  "category": "dining",
+  "memo": "This bbq is legit!. Also, i eat out a lot.",
+  "timeStamp": "2016-11-06T18:25:43.511Z",
+  "tags": [
+    "dining",
+    "lunch"
+  ]
+}, {
+  "kind": "transaction",
+  "amount": 1234.56,
+  "flow": "in",
+  "payee": "State of Nebraska",
+  "account": "joint checking",
+  "category": "income",
+  "memo": "Pay day is always awesome!",
+  "timeStamp": "2016-10-02T18:25:43.511Z",
+  "tags": [
+    "income"
+  ]
+}, {
+  "kind": "transaction",
+  "amount": 142.86,
+  "flow": "out",
+  "payee": "Wooly Canada",
+  "account": "joint checking",
+  "category": "clothing",
+  "memo": "Amazing wool sweater",
+  "timeStamp": "2016-09-22T18:25:43.511Z",
+  "tags": [
+    "trip",
+    "canada",
+    "clothes"
+  ]
+}, {
+  "kind": "transaction",
+  "amount": 72.11,
+  "flow": "out",
+  "payee": "Delicious Canada",
+  "account": "joint checking",
+  "category": "travel",
+  "memo": "Great meal - fresh salmon",
+  "timeStamp": "2016-06-23T18:25:43.511Z",
+  "tags": [
+    "trip",
+    "canada",
+    "dining"
   ]
 }, {
   "kind": "transaction",
