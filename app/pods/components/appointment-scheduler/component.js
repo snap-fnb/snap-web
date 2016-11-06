cbd9f6aa050367aafce88ac7dc2d0d7475e7908b
Tim Hemmer <themmer@d3banking.com>
2016-11-06 00:23:01 -0500

    Appointments through person and confirmation
    Day and Location is not done

--- app/pods/components/appointment-scheduler/component.js
+++ app/pods/components/appointment-scheduler/component.js
@@ -1,4 +1,62 @@
 import Ember from 'ember';
 
+const PERSON = 'Person';
+const LOCATION = 'Location';
+const DAY = 'Day';
+
 export default Ember.Component.extend({
+
+  /**
+   * filtered person
+   */
+  selectedPerson: null,
+
+  /**
+   * Once we filter we assume that is enough needed and continue down
+   */
+  isFiltered: Ember.computed('selectedPerson', {
+    get() {
+      const selectedPerson = this.get('selectedPerson');
+      return !!selectedPerson;
+    }
+  }),
+
+  /**
+   * Defaults to person if not passed
+   */
+  filterType: LOCATION,
+  filterTypes: Ember.A([LOCATION, PERSON, DAY]),
+
+  /**
+   * Simple booleans for display on filter types
+   */
+  isLocationFilterType: Ember.computed('filterType', {
+    get() {
+      const filterType = this.get('filterType');
+      return filterType === LOCATION;
+    }
+  }),
+
+  isDayFilterType: Ember.computed('filterType', {
+    get() {
+      const filterType = this.get('filterType');
+      return filterType === DAY;
+    }
+  }),
+
+  isPersonFilterType: Ember.computed('filterType', {
+    get() {
+      const filterType = this.get('filterType');
+      return filterType === PERSON;
+    }
+  }),
+
+  actions: {
+    selectFilterType(filterType) {
+      this.set('filterType', filterType);
+    },
+    selectPerson: function(selectedPerson) {
+      this.set('selectedPerson', selectedPerson);
+    }
+  }
 });
--- app/pods/components/person-list/component.js
+++ app/pods/components/person-list/component.js
@@ -1,4 +1,58 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  selectedPerson: null,
+
+  personList: [
+    {
+      name: 'Will DeRosear',
+      pictureUrl: 'https://www.firstnational.com/common/shared/e_communication/weekly-mortgage-news/images/william_derosear.jpg',
+      location1: 'Downtown Office',
+      location2: '1620 Dodge Street',
+      location3: 'Omaha, NE 68197',
+      office: '402-602-8923',
+      email: 'wderosear@fnni.com',
+      isAvailableNow: true
+    },
+    {
+      name: 'Amanda Barnes',
+      pictureUrl: 'https://www.firstnational.com/common/shared/e_communication/weekly-mortgage-news/images/amanda_barnes.jpg',
+      location1: '144th & Stony Brooke Branch',
+      location2: '5812 S. 144th Street',
+      location3: 'Omaha, NE 68137',
+      office: '402-602-2787',
+      email: 'abarnes@fnni.com',
+      isAvailableNow: false
+    },
+    {
+      name: 'Cory Beckius',
+      pictureUrl: 'https://www.firstnational.com/common/shared/e_communication/weekly-mortgage-news/images/cory_beckius.jpg',
+      location1: '167th & Giles Branch',
+      location2: '8311 S 167th Plaza',
+      location3: 'Omaha, NE 68136',
+      office: '402-602-4881',
+      email: 'cbeckius@fnni.com',
+      isAvailableNow: false
+    },
+    {
+      name: 'Karyn Randone',
+      pictureUrl: 'https://www.firstnational.com/common/shared/e_communication/weekly-mortgage-news/images/karyn_randone.jpg',
+      location1: '167th & Maple Branch',
+      location2: '16770 W Maple Road',
+      location3: 'Omaha, NE 68116',
+      office: '402-602-5394',
+      email: 'krandone@fnni.com',
+      isAvailableNow: false
+    }
+  ],
+  actions: {
+    selectPerson(person) {
+      this.get('selectPersonAction')(person);
+    },
+    chatWithPerson(person) {
+      // TODO activate Chat
+    }
+  }
+
 });
