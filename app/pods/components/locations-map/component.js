64dae72df52b557a0adc6cde5d1465fc3674307d
Tim Hemmer <themmer@d3banking.com>
2016-11-06 01:12:16 -0500

    Initial Maps login, but this uses geocoder (give me a city and it returns back lat long) instead of plotting on the map what we want

--- app/pods/components/locations-map/component.js
+++ app/pods/components/locations-map/component.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  maps: Ember.inject.service(),
+
+  location: 'First National Bank',
+
+  didInsertElement() {
+    this._super(...arguments);
+    let location = this.get('location');
+    let mapElement = this.get('maps').getMapElement(location);
+    this.$('.map-container').append(mapElement);
+  }
 });
