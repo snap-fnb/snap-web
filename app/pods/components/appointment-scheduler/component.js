3ce923b9f50051b62da0d97d25708cc763877bcc
Tim Hemmer <themmer@d3banking.com>
2016-11-06 01:55:52 -0500

    Date Picker is working with ember-power-calendar

--- app/pods/components/appointment-scheduler/component.js
+++ app/pods/components/appointment-scheduler/component.js
@@ -25,7 +25,7 @@ export default Ember.Component.extend({
    * Defaults to person if not passed
    */
   filterType: LOCATION,
-  filterTypes: Ember.A([LOCATION, PERSON, DAY]),
+  filterTypes: Ember.A([LOCATION, DAY, PERSON]),
 
   /**
    * Simple booleans for display on filter types
